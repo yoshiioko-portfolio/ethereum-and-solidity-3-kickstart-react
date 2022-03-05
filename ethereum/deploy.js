eb10e0e38f4e8082e4cefc84f60c90cf2cdbfd6e
yoshiioko <acardoza@pdx.edu>
2022-03-04 23:24:21 -0800

    Update: implement deploy script to Rinkeby

---
+++ ethereum/deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "smoke gravity lobster spot exhibit left satoshi leader auction renew room system",
+  // remember to change this to your own phrase!
+  "https://rinkeby.infura.io/v3/13ecc6bbfa6e406295059121a88d2629"
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
