360cd23be17ea64a9f1c2730d77a3501c47875dd
yoshiioko <acardoza@pdx.edu>
2022-03-08 21:30:50 -0800

    Update: wrap content in container to add gutter to sides. Add call for Header component.

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
+import Header from "./Header";
 
 const Layout = (props) => {
   return (
-    <div>
-      <h1>I'm a header</h1>
+    <Container>
+      <Header />
       {props.children}
       <h1>I'm a footer</h1>
-    </div>
+    </Container>
   );
 };
 
