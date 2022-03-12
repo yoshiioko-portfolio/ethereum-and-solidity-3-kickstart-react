c9f350656db4888f41a058018c406dfd96a40e27
yoshiioko <acardoza@pdx.edu>
2022-03-12 09:43:22 -0800

    Add: creates component CampaignNew that renders a form for users to create new campaigns

---
+++ pages/campaigns/new.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Form, Button } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+
+class CampaignNew extends Component {
+  render() {
+    return (
+      <Layout>
+        <h3>Create a Campaign!</h3>
+
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <input />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
+  }
+}
+
+export default CampaignNew;
