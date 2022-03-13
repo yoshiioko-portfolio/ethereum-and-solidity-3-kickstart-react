4865e4074b7cab8f6241f217c4763e2c7610fc52
yoshiioko <acardoza@pdx.edu>
2022-03-13 12:40:12 -0700

    Update: add query address to inital props. pass this to ContributeForm.

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -14,6 +14,7 @@ class CampaignShow extends Component {
     console.log(summary);
 
     return {
+      address: props.query.address,
       minimumContribution: summary[0],
       balance: summary[1],
       requestsCount: summary[2],
@@ -75,7 +76,7 @@ class CampaignShow extends Component {
           <Grid.Column width={10}>{this.renderCards()}</Grid.Column>
 
           <Grid.Column width={6}>
-            <ContributeForm />
+            <ContributeForm address={this.props.address} />
           </Grid.Column>
         </Grid>
       </Layout>
